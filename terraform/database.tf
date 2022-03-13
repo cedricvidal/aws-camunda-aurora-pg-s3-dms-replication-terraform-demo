a479b6fb51d46496ec1f9379908d09dfd81abde6
Cédric Vidal <cedric@vidal.biz>
2022-03-12 22:23:52 -0800

    Replaced RDS PG by Aurora PG cluster

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,33 +1,62 @@
-# Create Database Subnet Group
-# terraform aws db subnet group
-resource "aws_db_subnet_group" "demo-database-subnet-group-1" {
-    name         = "demo-database-subnet-group-1"
-    subnet_ids   = [aws_subnet.demo-subnet-public-1.id, aws_subnet.demo-subnet-public-2.id]
-    description  = "Subnets for Demo Database Instance 1"
+module "cluster" {
+  source  = "terraform-aws-modules/rds-aurora/aws"
 
-    tags   = {
-        Name = "demo-database-subnet-group-1"
-    }
+  name           = "demo-aurora-db-postgres-1"
+  engine         = "aurora-postgresql"
+  engine_version = "11.12"
+  instance_class = "db.r6g.large"
+  instances = {
+    one = {}
+    two = {}
+  }
+
+  vpc_id  = "${aws_vpc.demo-vpc-1.id}"
+  subnets = ["${aws_subnet.demo-subnet-public-1.id}", "${aws_subnet.demo-subnet-public-2.id}"]
+
+  allowed_security_groups = [aws_security_group.demo-sg-1.id]
+  allowed_cidr_blocks     = ["${aws_vpc.demo-vpc-1.cidr_block}"]
+
+  storage_encrypted   = true
+  apply_immediately   = true
+  monitoring_interval = 10
+
+  db_parameter_group_name         = "${aws_db_parameter_group.demo-aurora-db-postgres11-parameter-group.name}"
+  db_cluster_parameter_group_name = "${aws_rds_cluster_parameter_group.demo-rds-cluster-parameter-group-1.name}"
+
+  enabled_cloudwatch_logs_exports = ["postgresql"]
+
+  # Disable creation of random password - AWS API provides the password
+  create_random_password = false
+
+  master_username = "demo"
+  master_password = "th1s1s2d3m0Dms"
+
+  # Disable creation of subnet group - provide a subnet group
+  create_db_subnet_group = false
+
+  # Disable creation of security group - provide a security group
+  create_security_group = false
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
 
-# Get the Latest DB Snapshot
-# terraform aws data db snapshot
-#data "aws_db_snapshot" "latest-db-snapshot" {
-#    db_snapshot_identifier = "${var.demo-database-snapshot-identifier}"
-#    most_recent            = true
-#    snapshot_type          = "manual"
-#}
+resource "aws_db_parameter_group" "demo-aurora-db-postgres11-parameter-group" {
+  name        = "demo-aurora-db-postgres11-parameter-group"
+  family      = "aurora-postgresql11"
+  description = "demo-aurora-db-postgres11-parameter-group"
+  tags        = {
+      Name = "demo-aurora-db-postgres11-parameter-group"
+  }
+}
 
-# Create Database Instance Restored from DB Snapshots
-# terraform aws db instance
-resource "aws_db_instance" "database-instance" {
-    instance_class          = "${var.demo-database-instance-class}"
-    allocated_storage       = 10
-    skip_final_snapshot     = true
-    availability_zone       = "us-west-1a"
-    identifier              = "demo-db-1"
-#    snapshot_identifier     = data.aws_db_snapshot.latest-db-snapshot.id
-    db_subnet_group_name    = aws_db_subnet_group.demo-database-subnet-group-1.name
-    multi_az                = false
-    vpc_security_group_ids  = [aws_security_group.demo-sg-1.id]
+resource "aws_rds_cluster_parameter_group" "demo-rds-cluster-parameter-group-1" {
+  name        = "demo-aurora-postgres11-cluster-parameter-group"
+  family      = "aurora-postgresql11"
+  description = "demo-aurora-postgres11-cluster-parameter-group"
+  tags        = {
+      Name = "demo-aurora-postgres11-cluster-parameter-group"
+  }
 }
