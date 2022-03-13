13d84fd29edc604d8b84e3b7982095097fd7212a
Cédric Vidal <cedric@vidal.biz>
2022-03-12 21:38:27 -0800

    RDS allocated storage

--- terraform/database.tf
+++ terraform/database.tf
@@ -2,7 +2,7 @@
 # terraform aws db subnet group
 resource "aws_db_subnet_group" "demo-database-subnet-group-1" {
     name         = "demo-database-subnet-group-1"
-    subnet_ids   = [aws_subnet.demo-subnet-public-1.id]
+    subnet_ids   = [aws_subnet.demo-subnet-public-1.id, aws_subnet.demo-subnet-public-2.id]
     description  = "Subnets for Demo Database Instance 1"
 
     tags   = {
@@ -22,6 +22,7 @@ resource "aws_db_subnet_group" "demo-database-subnet-group-1" {
 # terraform aws db instance
 resource "aws_db_instance" "database-instance" {
     instance_class          = "${var.demo-database-instance-class}"
+    allocated_storage       = 10
     skip_final_snapshot     = true
     availability_zone       = "us-west-1a"
     identifier              = "demo-db-1"
