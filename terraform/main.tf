797958f73023f19b67414b450e68571ab91df21c
Cédric Vidal <cedric@vidal.biz>
2022-03-12 20:24:30 -0800

    EC2 instance is in public subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,6 @@ provider "aws" {
     region = "us-west-1"
 }
 
-resource "aws_instance" "demo-instance-1" {
-    ami = "ami-01f87c43e618bf8f0" // Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
-    instance_type = "t2.micro"
-    key_name = "demo-key-1"
-    tags = {
-        "Name" = "demo-instance-1"
-    }
-}
-
 resource "aws_vpc" "demo-vpc-1" {
     cidr_block           = "172.31.0.0/16"
     instance_tenancy     = "default"
@@ -64,14 +55,23 @@ resource "aws_route_table" "demo-rtb-public-1" {
     }
 }
 
-
 # Creating Route Associations public subnets
-resource "aws_route_table_association" "dev-public-1-a" {
+resource "aws_route_table_association" "demo-rtba-public-1-a" {
     subnet_id      = aws_subnet.demo-subnet-public-1.id
     route_table_id = aws_route_table.demo-rtb-public-1.id
 }
 
-resource "aws_route_table_association" "dev-public-2-a" {
+resource "aws_route_table_association" "demo-rtba-public-2-a" {
     subnet_id      = aws_subnet.demo-subnet-public-2.id
     route_table_id = aws_route_table.demo-rtb-public-1.id
+}
+
+resource "aws_instance" "demo-instance-1" {
+    ami = "ami-01f87c43e618bf8f0" // Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.demo-subnet-public-1.id
+    key_name = "demo-key-1"
+    tags = {
+        "Name" = "demo-instance-1"
+    }
 }
